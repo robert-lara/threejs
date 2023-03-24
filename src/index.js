c0db82e693d29442b87b9ff878f0414c88856948
Robert Lara III <robert@moosara.com>
2023-03-24 17:17:30 -0500

    Add car model
    Added car model to display

--- src/index.js
+++ src/index.js
@@ -1,32 +1,69 @@
 import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
-function component() {
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-    const renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(renderer.domElement);
+const loader = new GLTFLoader();
 
-    const geometry = new THREE.BoxGeometry(1, 1, 1);
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    const cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
+loader.load('assets/free_1975_porsche_911_930_turbo.glb', function ( gltf ) {
 
-    camera.position.z = 5;
+    function component(argScene) {
+        const scene = new THREE.Scene();
+        scene.background = new THREE.Color( 0xffff00 );
+        const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    
+        const renderer = new THREE.WebGLRenderer();
+        renderer.toneMapping = THREE.ACESFilmicToneMapping;
+				renderer.toneMappingExposure = 1;
+				renderer.outputEncoding = THREE.sRGBEncoding;
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        document.body.appendChild(renderer.domElement);
+    
+        const geometry = new THREE.BoxGeometry(1, 1, 1);
+        const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+        var ambientLight = new THREE.AmbientLight( 0xffffff );
+        ambientLight.position.x += 50;
 
-    function animate() {
-        requestAnimationFrame(animate);
+        scene.add( ambientLight );
+        argScene.position.y = -1.2;
+        scene.add(argScene);
+        const light = new THREE.AmbientLight( 0x000000 ); // soft white light
+        light.position.x += 1;
+        light.position.y += 1;
+        
+        scene.add( light );
 
-        cube.rotation.x += 0.01;
-        cube.rotation.y += 0.01;
+    
+        camera.position.z = 5;
 
-        renderer.render(scene, camera);
-    };
+        function animate() {
+            requestAnimationFrame(animate);
+            
+            scene.traverse( child => {
 
-    animate();
+                if ( child.material ) child.material.metalness = 0;
+            
+            } );
 
-    return renderer.domElement;
-}
+            argScene.rotation.y += 0.001;
 
-document.body.appendChild(component());
+            light.rotation.x += 0.01;
+            light.rotation.y += 0.01;
+
+            renderer.render(scene, camera);
+        };
+    
+        animate();
+        
+        
+    
+        return renderer.domElement;
+    }
+
+    document.body.appendChild(component(gltf.scene));
+
+}, undefined, function ( error ) {
+
+    console.error( error );
+
+} );
+

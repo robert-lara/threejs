91689e43cffa07b831f1bf4d486c5e0a71647a50
Robert Lara III <robert@moosara.com>
2023-03-25 00:55:32 -0500

    Added Image Upscaling with a click

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import x2 from '@upscalerjs/esrgan-thick/2x';
 import Upscaler from "upscaler";
-const upscaler = new Upscaler();
+const upscaler = new Upscaler({
+    model:x2,
+  })
 const loader = new GLTFLoader();
 const button = document.getElementById('photo');
 
@@ -12,13 +15,18 @@ button.onclick = function(){
     upscaler.upscale(showCanvas.toDataURL()).then(upscaledImage => {
 
         var canvas = document.getElementById('car-canvas');
+        const innerWidth = 640;//window.innerWidth;
+        const innerHeight = 480;//window.innerHeight;
+        const finalDimensions = applyScale(innerHeight, innerWidth, 10)
+        canvas.height = finalDimensions.height;
+        canvas.width = finalDimensions.width;
         var context = canvas.getContext('2d');
-
+        
         var image = new Image();
-        image.height = canvas.clientHeight;
-        image.width = canvas.clientWidth;
+        image.height = finalDimensions.height;
+        image.width = finalDimensions.width;
         image.onload = function() {
-            context.drawImage(image, 0,0,canvas.clientWidth,canvas.clientHeight);
+            context.drawImage(image, 0,0,finalDimensions.width,finalDimensions.height);
         };
         image.src = upscaledImage;
         console.log('upscale');
@@ -26,12 +34,12 @@ button.onclick = function(){
     });
 };
 
-function applyScale(height, width){
+function applyScale(height, width, scale){
 
     const selectedHeight = height;
     const selectedWidth = width;
 
-    const pixels = selectedWidth * selectedHeight * 10/100;
+    const pixels = selectedWidth * selectedHeight * scale/100;
 
     const wRatio = selectedHeight / selectedWidth;
     const hRatio = selectedWidth / selectedHeight;
@@ -51,7 +59,7 @@ loader.load('assets/free_1975_porsche_911_930_turbo.glb', function ( gltf) {
         const innerWidth = 640;//window.innerWidth;
         const innerHeight = 480;//window.innerHeight;
 
-        const finalDimensions = applyScale(innerHeight,innerWidth);
+        const finalDimensions = applyScale(innerHeight,innerWidth,5);
         const scene = new THREE.Scene();
         scene.background = new THREE.Color( 0xffff00 );
 
